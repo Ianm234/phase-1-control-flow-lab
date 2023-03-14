a210286018f1829da0cf86981b6b79e47dc34b7b
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-14 21:25:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let result
+  if (Distance>2000 feet){
+  result='charges 30 dollars'
+  } else if (Distance>2500 feet){
+  result= 'no can do'
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  return Destination = 'NYC'? 'OK, sounds good':'No Go'
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch(tip){
+  case 'generous':
+     return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+     return 'Bye'
+     }
+  
 }
